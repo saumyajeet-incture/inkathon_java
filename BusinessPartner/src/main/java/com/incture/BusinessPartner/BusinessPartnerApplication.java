6d9ba7e766e2db9ccfe6da6c86e9553c594ce9b4
saumyajeet-incture <saumyajeet.sahoo@incture.com>
2020-07-07 21:59:53 +0530

    Initial Setup

---
+++ BusinessPartner/src/main/java/com/incture/BusinessPartner/BusinessPartnerApplication.java
@@ -0,0 +1,13 @@
+package com.incture.BusinessPartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessPartnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessPartnerApplication.class, args);
+	}
+
+}
