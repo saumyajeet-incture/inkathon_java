30745dd905e11942818a1576b599f036c147bc14
saumyajeet-incture <saumyajeet.sahoo@incture.com>
2020-07-19 11:20:57 +0530

    removed unused imports

--- MasterBUPA2/src/main/java/com/incture/MasterBUPA/service/implementation/MainBupaServiceImplementation.java
+++ MasterBUPA2/src/main/java/com/incture/MasterBUPA/service/implementation/MainBupaServiceImplementation.java
@@ -1,7 +1,6 @@
 package com.incture.MasterBUPA.service.implementation;
 
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -33,8 +32,7 @@ import com.incture.MasterBUPA.service.abstraction.PaymentTransactionService;
 
 /**
  * @author ASHU
- * @author SOUMYAJEET
- *  Mapping the DTO Services to MODEL services 
+ * @author SOUMYAJEET Mapping the DTO Services to MODEL services
  */
 @Service
 @Transactional
@@ -46,10 +44,10 @@ public class MainBupaServiceImplementation implements MainBupaService {
 	// private PaymentTransactions payment = new PaymentTransactions();
 	// private Identification identity = new Identification();
 
-	public Integer bp_id,role_id;
+	public Integer bp_id, role_id;
 	public boolean status;
-	public List<Object[]> checkIfExists=null;
-	
+	public List<Object[]> checkIfExists = null;
+
 	public IdDto idDto;
 
 	/**
@@ -84,9 +82,9 @@ public class MainBupaServiceImplementation implements MainBupaService {
 
 	@Override
 	public IdDto save(SaveBupa saveBupa) {
+
+		idDto = new IdDto();
 		
-		idDto=new IdDto();
-		Object[] ids=null;
 
 		List<AddressDTO> addressDTO = saveBupa.getAddress();
 		BupaDTO bupaDTO = saveBupa.getBasicDetails();
@@ -96,78 +94,78 @@ public class MainBupaServiceImplementation implements MainBupaService {
 
 		BusinessPartner businessPartner = new BusinessPartner();
 		businessPartner.setBpId(0);
-		
+
 		BusinessPartner businessPartner2 = new BusinessPartner();
-		businessPartner2=BupaMapper.checkBP(bupaDTO);
-		
+		businessPartner2 = BupaMapper.checkBP(bupaDTO);
 
-		if(BupaMapper.checkBP(bupaDTO)!=null){
-			
-			
-		System.out.println("Inside if....");	
-		checkIfExists=bupaService.checkIfExists(BupaMapper.checkBP(bupaDTO).getBpRole(), BupaMapper.checkBP(bupaDTO).getFirstName(), BupaMapper.checkBP(bupaDTO).getLangKey(), BupaMapper.checkBP(bupaDTO).getLastName(), BupaMapper.checkBP(bupaDTO).getSearchTerm1(), BupaMapper.checkBP(bupaDTO).getSearchTerm2());
-		System.out.println("Checkif exists "+checkIfExists);
-		
-//		if(checkIfExists!=null){
-//		ids=checkIfExists.get(0);}
-		
-		if(checkIfExists.isEmpty()){
-			status=true;
-		}
-		else{
-			
-			System.out.println(Integer.valueOf( checkIfExists.get(0)[0].toString())+" "+Integer.valueOf( checkIfExists.get(0)[1].toString())+"bdkbhkdb");
-			status=false;
-		}
-		
-		System.out.println("Status is "+status);
-		
-		if(status==true){
-		businessPartner2.setRoleId((bupaService.findRoleId(businessPartner2.getBpRole()))+1);	
-//		businessPartner = bupaService.save(BupaMapper.checkBP(bupaDTO));
-		businessPartner = bupaService.save(businessPartner2);
-		}
-		
+		if (BupaMapper.checkBP(bupaDTO) != null) {
+
+			System.out.println("Inside if....");
+			checkIfExists = bupaService.checkIfExists(BupaMapper.checkBP(bupaDTO).getBpRole(),
+					BupaMapper.checkBP(bupaDTO).getFirstName(), BupaMapper.checkBP(bupaDTO).getLangKey(),
+					BupaMapper.checkBP(bupaDTO).getLastName(), BupaMapper.checkBP(bupaDTO).getSearchTerm1(),
+					BupaMapper.checkBP(bupaDTO).getSearchTerm2());
+			System.out.println("Checkif exists " + checkIfExists);
+
+			// if(checkIfExists!=null){
+			// ids=checkIfExists.get(0);}
+
+			if (checkIfExists.isEmpty()) {
+				status = true;
+			} else {
+
+				System.out.println(Integer.valueOf(checkIfExists.get(0)[0].toString()) + " "
+						+ Integer.valueOf(checkIfExists.get(0)[1].toString()) + "bdkbhkdb");
+				status = false;
+			}
+
+			System.out.println("Status is " + status);
+
+			if (status == true) {
+				businessPartner2.setRoleId((bupaService.findRoleId(businessPartner2.getBpRole())) + 1);
+				// businessPartner =
+				// bupaService.save(BupaMapper.checkBP(bupaDTO));
+				businessPartner = bupaService.save(businessPartner2);
+			}
+
 		}
 
-		if(status==true){
-		bp_id = businessPartner.getBpId();
-		role_id=businessPartner.getRoleId();
-		
-		
-		System.out.println("business partner id" + bp_id);
-		System.out.println("role id" + role_id);
-		
-		idDto.setBp_id(bp_id);
-		idDto.setRole_id(role_id);
-		idDto.setStatus(true);
+		if (status == true) {
+			bp_id = businessPartner.getBpId();
+			role_id = businessPartner.getRoleId();
+
+			System.out.println("business partner id" + bp_id);
+			System.out.println("role id" + role_id);
+
+			idDto.setBp_id(bp_id);
+			idDto.setRole_id(role_id);
+			idDto.setStatus(true);
 		}
-		
-		else{
-			
-			System.out.println("You are in else part and status is "+status);
-			role_id=Integer.valueOf( checkIfExists.get(0)[0].toString());
-			bp_id=Integer.valueOf( checkIfExists.get(0)[1].toString());
+
+		else {
+
+			System.out.println("You are in else part and status is " + status);
+			role_id = Integer.valueOf(checkIfExists.get(0)[0].toString());
+			bp_id = Integer.valueOf(checkIfExists.get(0)[1].toString());
 			idDto.setBp_id(bp_id);
 			idDto.setRole_id(role_id);
 			idDto.setStatus(false);
 			return idDto;
 		}
-		
-		
+
 		List<Address> address = AddressMapper.checkAddress(addressDTO, bp_id);
 		for (Address address2 : address) {
 			addressService.save(address2);
 		}
 
-		if(CommunicationMapper.checkCommunication(communicationDTO, bp_id)!=null){
-		communicationService.save(CommunicationMapper.checkCommunication(communicationDTO, bp_id));}
-
-		if(IdentificationMapper.checkIdentification(identificationDTO, bp_id)!=null){
-		identificationService.save(IdentificationMapper.checkIdentification(identificationDTO, bp_id));
+		if (CommunicationMapper.checkCommunication(communicationDTO, bp_id) != null) {
+			communicationService.save(CommunicationMapper.checkCommunication(communicationDTO, bp_id));
 		}
 
-		
+		if (IdentificationMapper.checkIdentification(identificationDTO, bp_id) != null) {
+			identificationService.save(IdentificationMapper.checkIdentification(identificationDTO, bp_id));
+		}
+
 		List<PaymentTransactions> paymentList = PaymentMapper.checkPayment(paymentDTO, bp_id);
 		for (PaymentTransactions pt : paymentList) {
 			paymentTransactionService.save(pt);
